61b7732fe6715f3e766a856f5de3b647c1dab14c
Frex Cuadillera <fcuadillera@gmail.com>
2021-02-07 19:55:04 +0800

    Create Arduino_Pro_Mini_code.ino

---
+++ February 6, 2021/Arduino_Pro_Mini_code/Arduino_Pro_Mini_code.ino
@@ -0,0 +1,62 @@
+#include <SPI.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 accelgyro;
+int16_t ax, ay, az;
+
+int soundLevel;
+
+String str_ax, str_ay, str_az, str_soundLevel, string_buffer;
+
+#define OUTPUT_READABLE_ACCELGYRO
+
+#define SOUND_PIN A2
+
+void setup() {
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(115200);
+
+    // initialize device
+    accelgyro.initialize();
+
+    //set offsets
+    
+    accelgyro.setXAccelOffset(-6573);
+    accelgyro.setYAccelOffset(1740);
+    accelgyro.setZAccelOffset(1449);
+
+    accelgyro.setXGyroOffset(26);
+    accelgyro.setYGyroOffset(-17);
+    accelgyro.setZGyroOffset(3);
+
+    //sound sensor
+
+    pinMode(SOUND_PIN, INPUT);
+
+}
+
+void loop() {
+    accelgyro.getAcceleration(&ax, &ay, &az);
+
+    soundLevel = analogRead(SOUND_PIN);
+
+    str_ax = String(ax);
+    str_ay = String(ay);
+    str_az = String(az);
+    str_soundLevel = String(soundLevel);
+
+    string_buffer = str_ax + " " + str_ay + " " + str_az + " " + str_soundLevel;
+    
+    Serial.println(string_buffer);
+
+}
